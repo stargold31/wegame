de7e2e6fd867271ca26c97bbba8c69d86a521740
stargold31 <ilia.nikulin07@gmail.com>
2024-10-08 20:58:55 +0700

    общая оболочка

---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Console.writeLine("ldf;ljdmgl;");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
